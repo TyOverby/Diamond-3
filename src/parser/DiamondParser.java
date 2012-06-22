29ca5fe703ccc2e69a7b1e1d7fa04ad5852eadf9
Meyer Kizner <meyer@pre-alpha.com>
2012-06-22 06:34:33 -0400

    Some more parser outline

--- src/parser/DiamondParser.java
+++ src/parser/DiamondParser.java
@@ -1,5 +1,6 @@
 package parser;
 
+import com.google.common.collect.Lists;
 import lexer.DiamondLexer.Lexeme;
 import lexer.Token;
 
@@ -11,18 +12,41 @@ public final class DiamondParser {
     public DiamondParser() {
     }
 
-    public CompilationUnit parse(List<Token<Lexeme>> tokens) {
+    public CompilationUnit parse(List<Token<Lexeme>> tokens) throws ParseException {
         CompilationUnit compilationUnit = new CompilationUnit();
         current = compilationUnit;
 
+        List<Token<Lexeme>> nextNode = Lists.newArrayList();
         for (Token<Lexeme> token : tokens) {
             switch (token.lexeme) {
-                // do stuff here
-                default:
-                    throw new UnsupportedOperationException("unknown token");
+                case LEFT_BRACE:
+                    current = parseStatement(nextNode);
+                    nextNode.clear();
+                    break;
+                case RIGHT_BRACE:
+                    if (!nextNode.isEmpty()) {
+                        throw new ParseException("expected ';' or '{'");
+                    }
+                    current = current.getParent();
+                    nextNode.clear();
+                    break;
+                case SEMICOLON:
+                    parseExpression(nextNode);
+                    nextNode.clear();
+                    break;
             }
         }
 
+        if (current != compilationUnit) {
+            throw new ParseException("expected '}'");
+        }
+
         return compilationUnit;
+    }
+
+    private Statement parseStatement(List<Token<Lexeme>> statement) {
+    }
+
+    private Expression parseExpression(List<Token<Lexeme>> expression) {
     }
 }
